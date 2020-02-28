562a7e8a75d115c87521c9d868c011ee5e8758f4
Micah Banschick <micha.ban@gmail.com>
2020-02-28 18:09:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.filter{|word| word[0..1].match(/un|Un/) != nil && word[word.length - 3...word.length].match(/ing/) != nil}
+  text.split(" ").filter{|word| word[0..1].match(/un|Un/) != nil && word[word.length - 3...word.length].match(/ing/) != nil}
 end
 
 def words_five_letters_long(text)
